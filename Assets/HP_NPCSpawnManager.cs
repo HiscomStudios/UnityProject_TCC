201aeedf5445cc63a843a207659ca433585149d9
Lucas Martin <lucasmartinmacedo@gmail.com>
2023-03-15 10:28:52 -0300

    Changelog:
    - Criação do projeto do TCC na Unity
    - Adição da engine da Hiscom

---
+++ Assets/HP_NPCSpawnManager.cs
@@ -0,0 +1,57 @@
+namespace HiscomProject.Scripts.Patterns.MMVCC.Managers
+{
+    using Unity.VisualScripting;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using Views;
+    
+    public class HP_NPCSpawnManager : MonoBehaviour
+    {
+        #region Variables
+
+        #region Private Variables
+        
+        public List<HP_NPCView> availableNPCs;
+        public List<HP_NPCView> sessionNPCs;
+
+        #endregion
+
+        #endregion
+
+        #region Methods
+
+        #region Private Methods
+
+        private void Setup()
+        {
+            sessionNPCs = new List<HP_NPCView>();
+            foreach (var availableNPC in availableNPCs)
+                sessionNPCs.Add(availableNPC);
+        }
+
+        #endregion
+
+        #endregion
+        
+        #region Singleton
+        
+        private static HP_NPCSpawnManager _instance;
+        public static HP_NPCSpawnManager Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+
+                var go = Instantiate(Resources.Load("Managers/HP_NPCSpawnManager"));
+                go.name = "@HP_NPCSpawnManager";
+                _instance = go.GetComponent<HP_NPCSpawnManager>();
+                _instance.Setup();
+                
+                DontDestroyOnLoad(go); 
+                return _instance;
+            }
+        }
+
+        #endregion
+    }
+}
